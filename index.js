12030822974852bd216b12df3b577cdbbd79d4f0
Vitalii <vittiger09@gmail.com>
2022-01-22 22:35:00 +0300

    NodeJS HW-1

---
+++ index.js
@@ -0,0 +1,35 @@
+const yargs = require("yargs");
+const { addNote, printNotes, removeNote } = require("./notes.controller");
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove note by id",
+  handler({ id }) {
+    removeNote(id);
+  },
+});
+
+yargs.parse();
---
+++ notes.controller.js
@@ -0,0 +1,43 @@
+const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+  const note = {
+    title,
+    id: Date.now().toString(),
+  };
+  notes.push(note);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.green("Note was added"));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.blue("There is the list of notes"));
+  notes.forEach((note) => {
+    console.log(chalk.cyan(note.id, note.title));
+  });
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const newArray = notes.filter((n) => n.id !== id.toString());
+
+  await fs.writeFile(notesPath, JSON.stringify(newArray));
+}
+
+module.exports = {
+  addNote,
+  printNotes,
+  removeNote,
+};
